4953b3842e6653d3b6411c01449e63f36602b177
mauxmee <mauxmee@gmail.com>
2021-05-11 18:21:10 +1000

    first check in

---
+++ src/main/java/com/example/demopost/DemoPostApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demopost;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoPostApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
